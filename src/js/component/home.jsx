ec0c1fd4e9388b9dea24d3cc53704071e4a9d90c
Almudena De La Rosa <almudenaxico@gmail.com>
2021-08-18 17:42:45 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,28 @@
-import React, { userState } from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 //import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [selectedColor, setSelectedColor] = userState("yellow");
+	const [selectedColor, setSelectedColor] = useState("red");
 
 	return (
 		<div className="traffic-ligth">
 			<div
+				onClick={() => setSelectedColor("red")}
 				className={
-					"ligth red" + (selectedColor === "red" ? "glow" : "")
+					"ligth red " + (selectedColor === "red" ? "glow" : "")
 				}></div>
 			<div
+				onClick={() => setSelectedColor("yellow")}
 				className={
-					"ligth yellow" + (selectedColor === "yellow" ? "glow" : "")
+					"ligth yellow " + (selectedColor === "yellow" ? "glow" : "")
 				}></div>
 			<div
+				onClick={() => setSelectedColor("green")}
 				className={
-					"ligth green" + (selectedColor === "green" ? "glow" : "")
+					"ligth green " + (selectedColor === "green" ? "glow" : "")
 				}></div>
 		</div>
 	);
